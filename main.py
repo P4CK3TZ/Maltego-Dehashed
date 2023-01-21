bb2f36fa2c63bc8bb5fa216bc64ab35de0dc7737
P4CK3TZ - OSIG <109151664+P4CK3TZ@users.noreply.github.com>
2023-01-21 02:31:33 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import sys
+from MaltegoTransform import *
+
+# Create a Maltego transform object
+m = MaltegoTransform()
+
+# Get the email address from the Maltego entity
+email = m.getVar("email")
+
+def check_email(email):
+    # Make an API call to the HaveIBeenPwned API
+    response = requests.get(f'https://haveibeenpwned.com/api/v3/breachedaccount/{email}')
+    if response.status_code == 200:
+        breaches = response.json()
+        for breach in breaches:
+            # Add a new entity to the Maltego graph for each breach
+            m.addEntity("maltego.Breach", breach["Name"])
+            search_dehashed(email)
+    else:
+        m.addUIMessage("The email address has not been compromised.")
+
+def search_dehashed(email):
+    # Make an API call to the Dehashed API
+    api_key = 'YOUR_API_KEY'
+    response = requests.get(f'https://api.dehashed.com/search?query={email}&api_key={api_key}')
+    if response.status_code == 200:
+        data = response.json()
+        if data.get('records'):
+            for record in data['records']:
+                # Add a new entity to the Maltego graph for each breach found in dehashed
+                m.addEntity("maltego.Breach", record['breach'])
+    else:
+        m.addUIMessage("Error: Unable to fetch data from Dehashed API.")
+
+check_email(email)
+
+# Return the Maltego graph to the user
+m.returnOutput()
